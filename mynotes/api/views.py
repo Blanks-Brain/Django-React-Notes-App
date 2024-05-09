c444329c1459db3212c325ccdedd6e34bf7016cf
Jobaer Ahamed <jobaer.ahamed@brainstation-23.com>
2024-05-09 12:24:48 +0600

    add crud for forntend

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -5,11 +5,11 @@ from .models import Notes
 # Create your views here.
 
 class ListCreateAPIView(ListCreateAPIView):
-    queryset = Notes.objects.all()
+    queryset = Notes.objects.all().order_by('-updated_at')
     serializer_class = NoteSerializer
 
 class RUDAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Notes.objects.all()
+    queryset = Notes.objects.all().order_by('-updated_at')
     serializer_class = NoteSerializer
 
     
